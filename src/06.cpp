6f6a7d8d2e8459d8adb4a4177596c60444871fbc
Lookins01 <lookins1q@gmail.com>
2025-03-01 12:05:34 +0500

    [style] fix indentation for 6th problem

--- src/06.cpp
+++ src/06.cpp
@@ -6,12 +6,15 @@
 
 int main(void)
 {
-    std::string task = "The sum of the squares of the first ten natural numbers is,\r\n"
-"    1^2 + 2^2 + ... + 10^2 = 385.\r\n"
-"The square of the sum of the first ten natural numbers is,\r\n"
-"    (1 + 2 + ... + 10)^2 = 55^2 = 3025.\r\n"
-"Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.\r\n"
-"Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.\r\n";
+    std::string task = 
+        "The sum of the squares of the first ten natural numbers is,\r\n"
+        "    1^2 + 2^2 + ... + 10^2 = 385.\r\n"
+        "The square of the sum of the first ten natural numbers is,\r\n"
+        "    (1 + 2 + ... + 10)^2 = 55^2 = 3025.\r\n"
+        "Hence the difference between the sum of the squares of the first ten "
+        "natural numbers and the square of the sum is 3025 − 385 = 2640.\r\n"
+        "Find the difference between the sum of the squares of the first "
+        "one hundred natural numbers and the square of the sum.";
 
     const int max = 100;
 
